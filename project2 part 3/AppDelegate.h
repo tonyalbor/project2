778f3c0b7ab80a3241d3d14f8102d95d991edb97
Tony Albor <talbo001@ucr.edu>
2013-09-08 18:13:18 -0700

    Initial Commit

---
+++ project2 part 3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  project2 part 3
+//
+//  Created by Tony Albor on 9/8/13.
+//  Copyright (c) 2013 tonyalbor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ project2 part 3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  project2 part 3
+//
+//  Created by Tony Albor on 9/8/13.
+//  Copyright (c) 2013 tonyalbor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
