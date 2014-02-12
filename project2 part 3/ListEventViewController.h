74d6deb6d55874e0812aec353223566a73cd313e
Tony Albor <talbo001@ucr.edu>
2014-02-12 02:39:34 -0800

    added datasources

--- project2 part 3/ListEventViewController.h
+++ project2 part 3/ListEventViewController.h
@@ -9,14 +9,20 @@
 #import <UIKit/UIKit.h>
 #import "ListEvent.h"
 #import "ListEventCell.h"
-#import "ListEventDataSource.h"
 #import "CustomCellColor.h"
 #import "DetailViewController.h"
+
+@class ListEventDataSource;
+@class CompletedDataSource;
+@class DeletedDataSource;
 
 @interface ListEventViewController : UIViewController <UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
-@property (strong, nonatomic) ListEventDataSource *sharedDataSource;
+
+@property (strong, nonatomic) ListEventDataSource *eventDataSource;
+@property (strong, nonatomic) CompletedDataSource *completedDataSource;
+@property (strong, nonatomic) DeletedDataSource *deletedDataSource;
 
 @property (weak, nonatomic) IBOutlet UIView *containerView;
 
