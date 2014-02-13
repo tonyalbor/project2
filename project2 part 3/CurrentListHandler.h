fb4ad2b6e469e7a33ac10059a1f14acb1e7083da
Tony Albor <talbo001@ucr.edu>
2014-02-12 21:41:26 -0800

    added CurrentListHandler

---
+++ project2 part 3/CurrentListHandler.h
@@ -0,0 +1,28 @@
+//
+//  CurrentListHandler.h
+//  project2 part 3
+//
+//  Created by Tony Albor on 2/12/14.
+//  Copyright (c) 2014 tonyalbor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class ListEventDataSource;
+@class CompletedDataSource;
+@class DeletedDataSource;
+
+@interface CurrentListHandler : NSObject
+
+// shared data source to be used throughout the entire app
++ (CurrentListHandler *)sharedDataSource;
+
+// 0 - deleted
+// 1 - events
+// 2 - completed
+@property (strong, nonatomic) NSNumber *currentList;
+
+// returns the data source for the current list
+- (id)currentListDataSource;
+
+@end
---
+++ project2 part 3/CurrentListHandler.m
@@ -0,0 +1,52 @@
+//
+//  CurrentListHandler.m
+//  project2 part 3
+//
+//  Created by Tony Albor on 2/12/14.
+//  Copyright (c) 2014 tonyalbor. All rights reserved.
+//
+
+#import "CurrentListHandler.h"
+#import "ListEventDataSource.h"
+#import "CompletedDataSource.h"
+#import "DeletedDataSource.h"
+
+@implementation CurrentListHandler
+
+@synthesize currentList;
+
+static CurrentListHandler *_sharedDataSource = nil;
+
++ (CurrentListHandler *)sharedDataSource {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _sharedDataSource = [[CurrentListHandler alloc] init];
+        _sharedDataSource.currentList = @1;
+    });
+    
+    return _sharedDataSource;
+}
+
+- (void)setCurrentList:(NSNumber *)list {
+    currentList = list;
+}
+
+- (id)currentListDataSource {
+    int list = currentList.intValue;
+    
+    switch(list) {
+        case 0:
+            return [DeletedDataSource sharedDataSource];
+        case 1:
+            return [ListEventDataSource sharedDataSource];
+        case 2:
+            return [CompletedDataSource sharedDataSource];
+        default:
+            // what
+            return nil;
+    }
+    
+    return nil;
+}
+
+@end
