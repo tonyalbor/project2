461a754107177a0310f50bb6456063c409bb84ec
Tony Albor <talbo001@ucr.edu>
2014-04-05 22:21:48 -0700

    initial commit

---
+++ project2 part 3/EventDataSource.h
@@ -0,0 +1,13 @@
+//
+//  EventDataSource.h
+//  project2 part 3
+//
+//  Created by Tony Albor on 4/5/14.
+//  Copyright (c) 2014 tonyalbor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EventDataSource : NSObject
+
+@end
---
+++ project2 part 3/EventDataSource.m
@@ -0,0 +1,13 @@
+//
+//  EventDataSource.m
+//  project2 part 3
+//
+//  Created by Tony Albor on 4/5/14.
+//  Copyright (c) 2014 tonyalbor. All rights reserved.
+//
+
+#import "EventDataSource.h"
+
+@implementation EventDataSource
+
+@end
